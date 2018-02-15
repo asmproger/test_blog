1d48af4c97ff2ac18742ac27d36e5e36b819b41f
sovkutsan <asmprogr@gmail.com>
2018-02-15 12:52:20 +0600

    fosuserbundle settings & model

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sovkutsan
+ * Date: 2/15/18
+ * Time: 12:27 PM
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\userBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="blg_user")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="auto)
+     *
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+}
