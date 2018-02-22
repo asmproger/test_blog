5dbaee16dddfb31700a38ea1aa0f8d4ee7a19831
sovkutsan <asmprogr@gmail.com>
2018-02-22 10:53:27 +0600

    angular blog (pririmitive pagination & view post)
    some code for another new branch %)

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -5,9 +5,13 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\BlogPost;
 use AppBundle\Entity\Page;
 use AppBundle\Service\GoogleParser;
+use Doctrine\DBAL\Types\TextType;
 use Knp\Component\Pager\Paginator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\QueryBuilder;
 
@@ -36,11 +40,29 @@ class BlogController extends Controller
         $query = $builder->getQuery();
 
         $p = $this->get('knp_paginator');
-        $paginator = $p->paginate( $query, $request->get('page', 1), 10);
+        $paginator = $p->paginate($query, $request->get('page', 1), 10);
         // replace this example code with whatever you need
         return $this->render('blog/index.html.twig', [
             'paginator' => $paginator,
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * #Route("/blog-add", name="blog_add")
+     */
+    public function addAction(Request $request)
+    {
+        $post = new BlogPost();
+        $form = $this->getForm($post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            die('FORM OK');
+        }
+        return $this->render('blog/add.html.twig', [
+            'form' => $form
         ]);
     }
 
@@ -60,5 +82,32 @@ class BlogController extends Controller
         return $this->render('blog/view.html.twig', [
             'item' => $item
         ]);
+    }
+
+    private function getForm(BlogPost $post) {
+        $builder = $this->createFormBuilder($post);
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'required' => 1
+            ])
+            ->add('short', TextareaType::class, [
+                'label' => 'Short description',
+                'required' => 1
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Description',
+                'required' => 1
+            ])
+            ->add('pic', FileType::class, [
+                'label' => 'Image:',
+                'required' => false,
+                'data_class' => null
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => $post->getId() ? 'Edit' : 'Add',
+            ]);
+        ;
+        return $builder->getForm();
     }
 }
--- web/js/ang.js
+++ web/js/ang.js
@@ -1,52 +1,127 @@
+// urll for api calls
+var apiUrl = 'http://test_blog.local/app_dev.php/api/v1/';
+var ipp = 10;
+var currentPage = 123;
+//new angular app
 var app = angular.module('blogApp', [
         'ngRoute'
     ])
         .config(function ($interpolateProvider, $locationProvider, $routeProvider) {
-            $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+            $interpolateProvider.startSymbol('{[{').endSymbol('}]}'); // replace {{ to {[{ (for twig)
 
+            // routes, smth like http://som_url#!/posts
             $locationProvider.hashPrefix('!');
-
-            $routeProvider.when('/posts', {
-                template: '<posts-list></posts-list>'
-            }).when('/posts/:id', {
-                template: '<post-detail></post-detail>'
-            }).otherwise('/posts');
+            $routeProvider
+                .when('/posts/:page/:ipp', {
+                    template: '<posts-list></posts-list>'
+                })
+                .when('/posts', {
+                    template: '<posts-list></posts-list>'
+                })
+                .when('/posts/:id', {
+                    template: '<post-detail></post-detail>'
+                })
+                .otherwise('/posts');
         })
 ;
 
+// component for posts list
 var component = app.component('postsList', {
     //template: 'OK, BRO',
-    templateUrl: '../test.template.html',
-    controller: ['$scope', '$http', function PostsListController($scope, $http) {
+    templateUrl: '../test.template.html', // using templateurl beacouse of routing, i can't do this another way
+    controller: ['$routeParams', '$scope', '$http', function PostsListController($routeParams, $scope, $http) {
         var self = this;
         this.test = 'OK';
-        $http.get('http://test_blog.local/app_dev.php/api/v1/blogs/1/10', {'Accept': 'application/json'}).then(function (response) {
+        var page = $routeParams.hasOwnProperty('page') ? $routeParams.page : 1;
+        var ipp = $routeParams.hasOwnProperty('ipp') ? $routeParams.ipp : 10;
+        console.log($routeParams.page + '_' + $routeParams.ipp);
+        console.log(page + '_' + ipp);
+        currentPage = page;
+        // i don't know hot to use here symfony routes %) it's api calls, ok?
+        $http.get(apiUrl + 'blogs/' + page + '/' + ipp, {'Accept': 'application/json'}).then(function (response) {
             $scope.items = response.data.items;
             self.items = response.data.items;
-            console.clear();
+            /*console.clear();
             console.log('Response here');
-            console.log(response.data);
+            console.log(response.data);*/
         }, function (response) {
             alert('err');
         });
     }]
 });
 
+// component for post details view
 var component_detail = app.component('postDetail', {
     //template: 'TBD: Detail view for <span>{{$ctrl.phoneId}}</span>',
     templateUrl: '../post.html',
     controller: ['$routeParams', '$http', function PostDetailController($routeParams, $http) {
         this.id = $routeParams.id;
+        console.log($routeParams);
         var self = this;
         this.test = 'OK';
-        $http.get('http://test_blog.local/app_dev.php/api/v1/blogs/' + this.id, {'Accept':'application/json'}).then(function(response) {
+        $http({
+            method: 'GET',
+            url: apiUrl + 'blogs/' + this.id,
+            headers: {
+                'Accept': 'application/json'
+            },
+            /*data: {
+                id: this.id
+            }*/
+        }).then(function (response) {
             self.item = response.data;
         }, function (response) {
             alert('err');
             console.log(arguments);
         });
+        /*$http.get(apiUrl + 'blogs/' + this.id, {'Accept': 'application/json'}).then(function (response) {
+            self.item = response.data;
+        }, function (response) {
+            alert('err');
+            console.log(arguments);
+        });*/
     }]
 });
+
+var component_paginator = app.component('customPag', {
+    //template: 'TBD: Detail view for <span>{{$ctrl.phoneId}}</span>',
+    templateUrl: '../custom_pag.html',
+    controller: ['$http', function PaginationController($http) {
+        var self = this;
+        this.pages_count = 1;
+        this.ipp = ipp;
+        this.current_page = currentPage;
+        this.url = apiUrl;
+        $http({
+            method: 'GET',
+            url: apiUrl + 'blogs-count',
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(function (response) {
+            self.pages_count = Math.ceil(response.data.count / ipp);
+            self.pages_links = Array();
+            for (var i = 0; i < self.pages_count; i++) {
+                self.pages_links[i] = {
+                    page: i + 1,
+                    href: /*apiUrl + */'posts/' + (i + 1) + '/' + ipp
+                };
+            }
+            console.log(self.pages_links);
+            self.item = response.data;
+        }, function (response) {
+            alert('err');
+            console.log(arguments);
+        });
+        /*$http.get(apiUrl + 'blogs/' + this.id, {'Accept': 'application/json'}).then(function (response) {
+            self.item = response.data;
+        }, function (response) {
+            alert('err');
+            console.log(arguments);
+        });*/
+    }]
+});
+
 // component_detail.controller('PostDetailController', function ($http, $routeParams) {
 //     alert('e');
 //     this.phoneId = $routeParams.phoneId;
