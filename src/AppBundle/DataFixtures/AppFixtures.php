761e879e99e4ad952b237a0343bdd300b3c5b9e7
sovkutsan <asmprogr@gmail.com>
2018-02-27 10:10:16 +0600

    fixtures

---
+++ src/AppBundle/DataFixtures/AppFixtures.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sovkutsan
+ * Date: 2/14/18
+ * Time: 1:59 PM
+ */
+
+namespace AppBundle\DataFixtures;
+
+use AppBundle\Entity\Page;
+use AppBundle\Entity\Setting;
+use AppBundle\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // test admin
+        $user = new User();
+        $user->setUsername('admin2');
+        $user->setUsernameCanonical('admin2');
+        $user->setEmail('asmproger@rambler.ru');
+        $user->setEmailCanonical('asmproger@rambler.ru');
+        $user->setEnabled(1);
+        $user->setRoles(['ROLE_SUPER_ADMIN']);
+
+        $pwd = $this->encoder->encodePassword($user, '123456');
+        $user->setPassword($pwd);
+
+        $manager->persist($user);
+
+        // some settings for blogposts task
+        $settings = [
+            'parser_task_period' => '5',
+            'parser_task_query' => 'xiaomi, fitness, intel',
+            'parser_task_last_update' => '1519030806',
+            'parser_task_count' => '5',
+            'parse_task_settings' => '{}'
+        ];
+        foreach ($settings as $k => $v) {
+            $setting = new Setting();
+            $setting->setSkey($k);
+            $setting->setValue($v);
+            $manager->persist($setting);
+        }
+
+        // some static pages
+        $pages = [
+            [
+                'title' => 'Contacts',
+                'body' => 'Contacts page',
+                'label' => 'contacts'
+            ],
+            [
+                'title' => 'About us',
+                'body' => 'Aobut us page. We are the champions, my friends!',
+                'label' => 'about_us'
+            ],
+            [
+                'title' => 'Our mission',
+                'body' => 'Our mission us page. Some water.',
+                'label' => 'our_mission'
+            ]
+        ];
+        foreach ($pages as $item) {
+            $page = new Page();
+            $page->setTitle($item['title']);
+            $page->setBody($item['body']);
+            $page->setShort($item['body']);
+            $page->setLabel($item['label']);
+
+            $manager->persist($page);
+        }
+
+        $manager->flush();
+    }
+}
